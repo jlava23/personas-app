762f0f50e224a440d1de5ca54abd17b5c6a33613
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-11-24 17:49:53 -0500

    Comuna List

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ComunaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
