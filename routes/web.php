4460a060b3a23041df20914ee4c2e4aeb7230300
Juanes2192 <juanestebanperdomoleiva@gmail.com>
2024-11-24 18:04:07 -0500

    Commune Add

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
 Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
+Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
+Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
+Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
+Route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
+Route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
